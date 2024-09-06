a22ad5238d032098091b33cb7399c4f704a41203
WheelchairBound64 <123192532+WheelchairBound64@users.noreply.github.com>
2024-09-06 01:51:49 -0500

    Changes Made
    Progressed project some

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Vector2 input;
+    private Rigidbody rb;
+    [SerializeField] float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Debug.Log(input);
+    }
+
+    private void FixedUpdate()
+    {
+
+        var newInput = GetCameraBasedInput(input, Camera.main);
+        var newVelocity = new Vector3(newInput.x * speed * Time.fixedDeltaTime, rb.velocity.y, newInput.z * speed * Time.fixedDeltaTime);
+
+        rb.velocity = newVelocity;
+    }
+
+    Vector3 GetCameraBasedInput(Vector2 input, Camera cam)
+    {
+        Vector3 camRight = cam.transform.right;
+        camRight.y = 0;
+        camRight.Normalize();
+
+        Vector3 camForward = cam.transform.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        return input.x * camRight + input.y * camForward;
     }
 }
